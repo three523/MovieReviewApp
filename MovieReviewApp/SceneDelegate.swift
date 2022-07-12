6e1d94983528ebaa7818cf2b086fc834d1c500e9
three523 <thtree523@gmail.com>
2022-07-12 17:41:32 +0900

    TabBarController 및 기본 화면 셋팅

--- MovieReviewApp/SceneDelegate.swift
+++ MovieReviewApp/SceneDelegate.swift
@@ -19,8 +19,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: scene)
-        window.rootViewController = 
+        window.rootViewController = TabBarController()
         window.makeKeyAndVisible()
+        self.window = window
         
     }
 
---
+++ MovieReviewApp/TabBarController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  MovieReviewApp
+//
+//  Created by apple on 2022/07/08.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBar.backgroundColor = .white
+        
+        let vc1 = HomeViewController()
+        let vc2 = SearchViewController()
+        let vc3 = ReviewViewController()
+        let vc4 = MyProfileViewController()
+        
+        vc1.title = "홈"
+        vc2.title = "검색"
+        vc3.title = "평가"
+        vc4.title = "나의 정보"
+        
+        self.setViewControllers([vc1, vc2, vc3, vc4], animated: false)
+        
+        guard let items = self.tabBar.items else { return }
+        
+        let images = ["house", "magnifyingglass", "star", "person.fill"]
+        
+        for i in 0..<items.count {
+            items[i].image = UIImage(systemName: images[i])
+        }
+        
+        self.modalPresentationStyle = .fullScreen
+    }
+
+
+}
+
