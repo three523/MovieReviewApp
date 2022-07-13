ecc3c95c1fd6e778a437592919b07ce7758e1be4
three523 <thtree523@gmail.com>
2022-07-13 18:21:24 +0900

    HomeViewController ui 구성 완료

--- MovieReviewApp/HomeViewController.swift
+++ MovieReviewApp/HomeViewController.swift
@@ -8,22 +8,97 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    let mainTableView: UITableView = UITableView(frame: .zero, style: .grouped)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        viewSetting()
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func viewSetting() {
+        
+        let toptabView: TopTabView = TopTabView()
+        toptabView.addButton(text: "영화")
+        toptabView.addButton(text: "TV 프로그램")
+        
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+        mainTableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.identifier)
+        
+        view.addSubview(toptabView)
+        view.addSubview(mainTableView)
+        
+        toptabView.translatesAutoresizingMaskIntoConstraints = false
+        let safeArea = view.safeAreaLayoutGuide
+        toptabView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10).isActive = true
+        toptabView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20).isActive = true
+        toptabView.trailingAnchor.constraint(lessThanOrEqualTo: safeArea.trailingAnchor, constant: -30).isActive = true
+        
+        mainTableView.translatesAutoresizingMaskIntoConstraints = false
+        mainTableView.topAnchor.constraint(equalTo: toptabView.bottomAnchor).isActive = true
+        mainTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        mainTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        mainTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
     }
-    */
 
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else if section == 1 {
+            return 1
+        } else {
+            return 1
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let returnedView = UIView(frame: CGRect(x: 0, y: 0, width: mainTableView.frame.size.width, height: 35))
+        returnedView.backgroundColor = .white
+
+        let label = UILabel(frame: CGRect(x: 5, y: 0, width: view.frame.size.width, height: 35))
+        label.font = UIFont(name: "AvenirNext-DemiBold", size: 20)
+        returnedView.addSubview(label)
+        
+        if section == 0 {
+            label.text = "TMDB 순위"
+        } else if section == 1 {
+            label.text = "TMDB 최고 평점"
+        } else {
+            label.text = "곧 나올 영화"
+        }
+        
+        return returnedView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 35
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as! HomeTableViewCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 280
+    }
+    
+    
 }
