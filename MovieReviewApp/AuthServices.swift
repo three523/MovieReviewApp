b2dc35815313b8c3552f72a86bf084f33212992c
three523 <71269216+three523@users.noreply.github.com>
2022-12-16 12:23:09 +0900

    Revert "Fix 폴더 정리"

--- MovieReviewApp/AuthServices.swift
+++ MovieReviewApp/AuthServices.swift
@@ -5,27 +5,27 @@
 //  Created by 김도현 on 2022/09/26.
 //
 
-//import Foundation
-//import AuthenticationServices
-//
-//protocol LoginManagerDelegate: AnyObject {
-//    
-//}
-//
-//final class LoginManager: NSObject {
-//    weak var vc: UIViewController?
-//    weak var delegate: LoginManagerDelegate?
-//    
-//    func setAppleLoginPresentationAnchorView(_ vc: UIViewController) {
-//        self.vc = vc
-//    }
-//}
-//
-//extension LoginManager: ASAuthorizationControllerPresentationContextProviding {
-//    
-//    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-//        guard let presentationAnchor = vc?.view.window else { return ASPresentationAnchor() }
-//        return presentationAnchor
-//    }
-//    
-//}
+import Foundation
+import AuthenticationServices
+
+protocol LoginManagerDelegate: AnyObject {
+    
+}
+
+final class LoginManager: NSObject {
+    weak var vc: UIViewController?
+    weak var delegate: LoginManagerDelegate?
+    
+    func setAppleLoginPresentationAnchorView(_ vc: UIViewController) {
+        self.vc = vc
+    }
+}
+
+extension LoginManager: ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        guard let presentationAnchor = vc?.view.window else { return ASPresentationAnchor() }
+        return presentationAnchor
+    }
+    
+}
